67ec3bb8748488b6c65fc287fbffe5c45fcd2dee
Diarsid <diarsid@gmail.com>
2021-11-21 18:44:18 +0200

    extensions improvement

--- src/main/java/diarsid/files/Extensions.java
+++ src/main/java/diarsid/files/Extensions.java
@@ -1,5 +1,7 @@
 package diarsid.files;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -20,8 +22,8 @@ public class Extensions {
         return this.extensionsByNames.computeIfAbsent(ext, (ext1) -> new Extension(ext));
     }
 
-    public Optional<Extension> getFor(File file) {
-        String extensionString = this.getExtensionStringFrom(file);
+    public Optional<Extension> getFor(String fileName) {
+        String extensionString = extensionStringOrNullFrom(fileName);
         if ( isNull(extensionString) || extensionString.isBlank() ) {
             return Optional.empty();
         }
@@ -31,14 +33,44 @@ public class Extensions {
         }
     }
 
-    private String getExtensionStringFrom(File file) {
-        int lastCommaIndex = file.name().lastIndexOf('.');
+    public Optional<Extension> getFor(Path path) {
+        if ( Files.isDirectory(path) ) {
+            return Optional.empty();
+        }
+
+        String extensionString = extensionStringOrNullFrom(path.getFileName().toString());
+        if ( isNull(extensionString) || extensionString.isBlank() ) {
+            return Optional.empty();
+        }
+        else {
+            Extension extension = this.extensionsByNames.computeIfAbsent(extensionString, Extension::new);
+            return Optional.of(extension);
+        }
+    }
+
+    public Optional<Extension> getFor(File file) {
+        String extensionString = this.extensionStringOrNullFrom(file);
+        if ( isNull(extensionString) || extensionString.isBlank() ) {
+            return Optional.empty();
+        }
+        else {
+            Extension extension = this.extensionsByNames.computeIfAbsent(extensionString, Extension::new);
+            return Optional.of(extension);
+        }
+    }
+
+    private static String extensionStringOrNullFrom(File file) {
+        return extensionStringOrNullFrom(file.name());
+    }
+
+    private static String extensionStringOrNullFrom(String fileName) {
+        int lastCommaIndex = fileName.lastIndexOf('.');
 
         if ( lastCommaIndex < 0 ) {
             return null;
         }
         else {
-            return file.name().substring(lastCommaIndex + 1);
+            return fileName.substring(lastCommaIndex + 1);
         }
     }
 }
